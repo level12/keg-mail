74d5186c7a0c4cdb3ee391dc24bf6d8802f85d8d
Matthias Hager <matthias.hager@level12.io>
2020-04-13 13:01:49 -0400

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup, find_packages
 
 cdir = osp.abspath(osp.dirname(__file__))
 README = open(osp.join(cdir, 'README.rst')).read()
-CHANGELOG = open(osp.join(cdir, 'CHANGELOG.rst')).read()
+CHANGELOG = open(osp.join(cdir, 'changelog.rst')).read()
+
+version = {}
+with open(osp.join(cdir, 'keg_mail', 'version.py')) as version_fp:
+    exec(version_fp.read(), version)
 
 setup(
     name="Keg-Mail",
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
     description="A mail sending library for keg applications",
     long_description='\n\n'.join((README, CHANGELOG)),
     author="Level 12 Developers",
@@ -19,14 +21,31 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     license='BSD',
     packages=find_packages(),
     zip_safe=True,
     install_requires=[
         'flask_mail',
+        'Keg',
+        'SQLAlchemy-Utils',
+        'arrow',
     ],
+    extras_require={
+        'mailgun': [
+            'requests',
+            'ordered-set',
+        ],
+        'test': [
+            'tox',
+            'pytest',
+            'pytest-coverage',
+            'requests-mock',
+            'flask-webtest',
+            'KegElements',
+        ]
+    }
 )
